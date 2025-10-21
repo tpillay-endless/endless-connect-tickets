9b6bcebee7192ebce61facecc4cd3b43f0b02681
Goran Komar <goran.komar@gmail.com>
2025-10-22 00:08:27 +0200

    API Ping Test

---
+++ src/app/api/ping/route.ts
@@ -0,0 +1,5 @@
+export async function GET() {
+  return new Response(JSON.stringify({ ok: true, msg: 'pong' }), {
+    headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-store' }
+  });
+}
